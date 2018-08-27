bca87050ea76b2c63b5c4a1ce6effc406e4e949c
kkwork <konrad.karasinski84@gmail.com>
2018-08-27 16:26:19 +0200

    Added month summary view. TODO: fix loop and formating

--- homeb_app/urls.py
+++ homeb_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.zakup_list, name='zakup_list'),
     path('zakup/edit/', views.zakup_nowy, name="zakup_nowy"),
     path('zakup/<int:pk>/', views.zakup_detail, name='zakup_detail'),
+    path('zakup/podsumowanie/', views.zakup_month, name='zakup_month'),
 ]
--- homeb_app/views.py
+++ homeb_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 import datetime
+from django.db.models import Sum
 
 from .forms import ZakupForm
-from .models import Zakup
+from .models import Zakup, Kategoria, Miesiac
 
 def zakup_list(request):
     '''zakupy = Zakup.objects.filter(category__name="jedzenie").values('price', 'date', 'name')'''
@@ -25,40 +26,57 @@ def zakup_nowy(request):
         form = ZakupForm()
     return render(request, 'homeb_app/zakup_edit.html', {'form': form})
 
+def zakup_month(request):
+    kategorie = Kategoria.objects.all()
+    miesiace = Miesiac.objects.all()
+    month_overall = [] 
+    totals = []
+    for miesiac in miesiace:
+        m = Zakup.objects.filter(month__name=miesiac).values('total').aggregate(Sum('total'))
+        print('\npodsumowanie miesiaca: \n', miesiac, ':', m, '\n')
+        month_overall.append(m)
+        for kategoria in kategorie:
+            k = (Zakup.objects.filter(month__name=miesiac, category=kategoria).values('category__name', 'total').aggregate(Sum('total')))
+            print(kategoria, ':', k)
+            totals.append(k)
+    return render(request, 'homeb_app/zakup_month.html', {'miesiace': miesiace, 'kategorie': kategorie, 'totals': totals})
+
 '''
 kategorie = Kategoria.objects.all()
-for kategoria in kategorie:
-    m = Zakup.objects.filter(month__name="sierpien", category=kategoria).values('category__name', 'total').aggregate(Sum('total'))
-    print(kategoria, ':', m)
+miesiace = Miesiac.objects.all()
 
+month_overall = [] 
+month_category_overall = []
 
+for miesiac in miesiace:
+    m = Zakup.objects.filter(month__name=miesiac).values('total').aggregate(Sum('total'))
+    print('\npodsumowanie miesiaca: \n', miesiac, ':', m, '\n')
+    month_overall.append(m)
+    for kategoria in kategorie:
+        k = (Zakup.objects.filter(month__name=miesiac, category=kategoria).values('category__name', 'total').aggregate(Sum('total')))
+        print(kategoria, ':', k)
+        month_category_overall.append(k)
+
+content[0]
+content[1]
+itd 
 '''
 
 
-#def zakup_month(request):
-    #m = Zakup.objects.filter(month__name="sierpien").values('total').aggregate(Sum('total'))
+'''
+    m = Zakup.objects.filter(month__name="sierpien").values('total').aggregate(Sum('total'))
 
 
-
-
-
-    #miesiace = Zakup.objects.all().values('month', 'price')
-    #for month in miesiace:
-        #zakupy_month = Zakup.objects.filter(month__name=month).values('price').aggregate(Sum('price'))
-      #ZZakup.objects.filter(month__name="sierpien").values('total').aggregate(Sum('total'))
-#Zakup.objects.all().delete()
-'''def login_view(request):
+    miesiace = Zakup.objects.all().values('month', 'price')
+    for month in miesiace:
+        zakupy_month = Zakup.objects.filter(month__name=month).values('price').aggregate(Sum('price'))
+        Zakup.objects.filter(month__name="sierpien").values('total').aggregate(Sum('total'))
+Zakup.objects.all().delete()
+'''
+'''
+def login_view(request):
     return render(request, 'registration/login.hml', {'form': login})
 
 def logout_view(request):
     return redirect(request, '/')
-'''
-
-'''
-months = Zakup.objects.all().values('month', 'price')
-for month in miesiace:
-    asd = sum(month.values())
-    print(month.values())
-    print(asd)
-
 '''
