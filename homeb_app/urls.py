fe5a66ba0bf46bf038e32f4ae2b61f6bc42ba3a0
kkwork <konrad.karasinski84@gmail.com>
2018-09-06 15:15:31 +0200

    Added last 10 feature

--- homeb_app/urls.py
+++ homeb_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.zakup_list, name='zakup_list'),
+    path('zakup/last/', views.zakup_last, name='zakup_last'),
     path('zakup/edit/', views.zakup_nowy, name="zakup_nowy"),
     path('zakup/<int:pk>/', views.zakup_detail, name='zakup_detail'),
     path('zakup/podsumowanie/', views.zakup_month, name='zakup_month'),
--- homeb_app/views.py
+++ homeb_app/views.py
@@ -10,9 +10,15 @@ def zakup_list(request):
     '''zakupy = Zakup.objects.filter(category__name="jedzenie").values('price', 'date', 'name')'''
     zakupy = Zakup.objects.all()
     return render(request, 'homeb_app/zakup_list.html', {'zakupy': zakupy})
+    
+def zakup_last(request):
+    last = Zakup.objects.filter(month=datetime.datetime.now().month).order_by('-id')[:10]
+    return render(request, 'homeb_app/zakup_last.html', {'last': last})
+
 def zakup_detail(request, pk):
     zakup = get_object_or_404(Zakup, pk=pk)
     return render(request, 'homeb_app/zakup_detail.html', {'zakup': zakup})
+
 def zakup_nowy(request):
     if request.method == "POST":
         form = ZakupForm(request.POST)
